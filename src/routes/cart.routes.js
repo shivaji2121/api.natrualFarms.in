c02b6eef90fb565272e7769b1072a36bc186194a
shivaji2121 <shivarkv123@gmail.com>
2025-10-27 13:20:44 +0530

    Addedd apis to cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -4,9 +4,17 @@ const cartController = require('../controllers/cart.controller');
 const cartValidation = require('../validations/cart.validator');
 const userMiddleware = require('../middlewares/auth.middleware');
 
-
 cartRouter.post('/add', userMiddleware.isAuthorized, cartValidation.addToCartValidation, cartController.addToCart);
+
 cartRouter.delete('/remove/:productId', userMiddleware.isAuthorized, cartValidation.addToCartValidation, cartController.removeFromCart);
+
+cartRouter.put('/update', userMiddleware.isAuthorized, cartValidation.updateCartValidation, cartController.updateCartItem);
+
+cartRouter.get('/', userMiddleware.isAuthorized, cartController.getCart);
+
+cartRouter.get('/count', userMiddleware.isAuthorized, cartController.getCartCount);
+
+cartRouter.delete('/clear', userMiddleware.isAuthorized, cartController.clearCart);
 
 
 module.exports = cartRouter;
